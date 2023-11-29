e62e41bd6d3ae4c0be760bb225f7ce1a9309e7dd
Riviox <100956266+RivioxGaming@users.noreply.github.com>
2023-11-29 17:00:04 +0100

    added changelog

--- assets/script.js
+++ assets/script.js
@@ -4,5 +4,14 @@ fetch('https://raw.githubusercontent.com/RivioxGaming/GalaxyFPS/main/version')
         document.getElementById('version').innerText = ver;
     })
     .catch(error => {
-        console.log('Error: ', error);
+        console.log('Version Error: ', error);
+});
+
+fetch('https://raw.githubusercontent.com/RivioxGaming/GalaxyFPS/main/changelog')
+    .then(response => response.text())
+    .then(clog => {
+        document.getElementById('clog').innerHTML = clog;
+    })
+    .catch(error => {
+        console.log('Changelog Error: ', error);
 });
